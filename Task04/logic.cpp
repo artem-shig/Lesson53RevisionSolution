7cbc42fbae6f291faa51d0e856081784c4b70bfc
artem-shig <artem.shigov@gmail.com>
2025-05-04 18:49:59 +0300

    Task04 solution

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -8,6 +8,69 @@
 
 #include "logic.h"
 
+int get_max_value(int** matrix, int n, int m) {
+
+	int max = matrix[0][0];
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 1; j < m; j++)
+		{
+			if (max < matrix[i][j]) {
+				max = matrix[i][j];
+			}
+		}
+	}
+	return max;
+}
+
+int get_min_value(int** matrix, int n, int m) {
+
+	int min = matrix[0][0];
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 1; j < m; j++)
+		{
+			if (min > matrix[i][j]) {
+				min = matrix[i][j];
+			}
+		}
+	}
+	return min;
+}
+
 int sum_elements_of_columns_with_extreme_value(int** matrix, int n, int m) {
-	return 0;
+
+	if (n <= 0 || m <= 0 || matrix == nullptr) {
+		return 0;
+	}
+
+	int sumAll = 0;
+	int sumСolumn;
+	int countExtremeValue;
+	int maxValue = get_max_value(matrix, n, m);
+	int minValue = get_min_value(matrix, n, m);
+
+
+	for (int j = 0; j < m; j++)
+	{
+		countExtremeValue = 0;
+		sumСolumn = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			sumСolumn += matrix[i][j];
+			if (matrix[i][j] == maxValue || matrix[i][j] == minValue) {
+				countExtremeValue++;
+			}
+		}
+
+		if (countExtremeValue == 0) {
+			sumСolumn = 0;
+		}
+		sumAll += sumСolumn;
+	}
+
+	return sumAll;
 }
