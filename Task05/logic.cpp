4502cbae0365b805e9a9c9e8abc52146a96a284a
artem-shig <artem.shigov@gmail.com>
2025-05-04 19:06:39 +0300

    Task05 solution

--- Task05/logic.cpp
+++ Task05/logic.cpp
@@ -8,4 +8,36 @@
 #include "logic.h"
 
 void get_last_local_minimum(int** matrix, int n, int m, int* ii, int* jj) {
+
+	if (n <= 1 || m <= 1 || matrix == nullptr) {
+		return;
+	}
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = m - 1; j >= 0; j--)
+		{
+			if (j == m - 1) {
+				if (matrix[i][j] < matrix[i][j - 1]) {
+					*ii = i;
+					*jj = j;
+					return;
+				}
+			}
+			else if (j < m - 1 && j > 0) {
+				if (matrix[i][j] < matrix[i][j - 1] && matrix[i][j] < matrix[i][j + 1]) {
+					*ii = i;
+					*jj = j;
+					return;
+				}
+			}
+			else if (j == 0) {
+				if (matrix[i][j] < matrix[i][j + 1]) {
+					*ii = i;
+					*jj = j;
+					return;
+				}
+			}
+		}
+	}
 }
